fbe63d7a33dfeb7493fe2d59eff34f4814652fd2
Bizzycola <lachlan@lchant.dev>
2025-03-23 15:03:06 +1000

    Update page title and add real favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,12 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import TrackingScript from '@/components/tracking';
 import "./globals.css";
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "LChantDev",
+  title: "Lachlan Chant",
   description: "Lachlan Chant's Developer Portfolio",
 };
 
